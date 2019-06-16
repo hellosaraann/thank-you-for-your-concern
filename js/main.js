c06f666023b388ab80bd13fc4a4b501d04813654
hellosaraann <srichardson@mcad.edu>
2019-06-15 20:18:24 -0500

    Added basic horizontally scrolling timeline

--- js/main.js
+++ js/main.js
@@ -1,34 +1,90 @@
-var widgetArray = [
+const widgetArray = [
   {id: '#timelineWidget'},
   {id: '#infographicWidget'},
   {id: '#gameWidget'},
   {id: '#storyWidget'}
 ];
 
+const timelineArray = [{
+    id: 1,
+    year: 'year',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+  },{
+    id: 2,
+    year: 'year',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+  },{
+    id: 3,
+    year: 'year',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+  },{
+    id: 4,
+    year: 'year',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+  },{
+    id: 5,
+    year: 'year',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+  },{
+    id: 6,
+    year: 'year',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+  },{
+    id: 7,
+    year: 'year',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+  },{
+    id: 8,
+    year: 'year',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+  },{
+    id: 9,
+    year: 'year',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+  },{
+    id: 10,
+    year: 'year',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+  },{
+    id: 11,
+    year: 'year',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+  },{
+    id: 12,
+    year: 'year',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+  },{
+    id: 13,
+    year: 'year',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+  },{
+    id: 14,
+    year: 'year',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+  },{
+    id: 15,
+    year: 'year',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
+  }
+];
+
+Vue.component ( 'timeline-entry', {
+	props: [
+		'events', 'id', 'text', 'year'
+	],
+	template: `
+			<div class='container column horizontal-scroll'>
+				<h2 class='timeline-image-placeholder'>{{ id }}</h2>
+        <h4 class='timeline-year'>{{ year }}</h4>
+				<p class='timeline-descriptors'>{{ text }}</p>
+			</div>
+	`
+});
+
 var vm = new Vue ({
   el: '#widgetApp',
   data: {
-    widgets: widgetArray
-  },
-  methods: {
-    // displayWidget: function(hoveredWidget){
-    //   hoveredWidget.classList.add('on-hover');
-    // },
-    // hideWidget: function(hoveredWidget){
-    //   hoveredWidget.classList.remove('on-hover');
-    // }
+    widgets: widgetArray,
+    events: timelineArray
   }
-})
-
-// document.addEventListener('DOMContentLoaded', function () {
-//     var wTrigger = document.querySelector('.widget-trigger');
-//     var widget = document.querySelector('.widget');
-//
-//     wTrigger.addEventListener('mouseenter', function () {
-//         widget.classList.add('on-hover');
-//     });
-//     wTrigger.addEventListener('mouseleave', function () {
-//         widget.classList.remove('on-hover');
-//     })
-//
-// })
+});
